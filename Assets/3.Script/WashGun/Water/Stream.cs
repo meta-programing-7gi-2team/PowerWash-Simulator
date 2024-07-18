5ae53c2d671b4b38fca6cc3e6c646d77791374af
신성진 <ssin72787@gmail.com>
2024-07-18 10:09:58 +0900

    [Update] Stream script 수정, stream prefab 등록

--- Assets/3.Script/WashGun/Water/Stream.cs
+++ Assets/3.Script/WashGun/Water/Stream.cs
@@ -5,31 +5,38 @@ using UnityEngine;
 public class Stream : MonoBehaviour
 {
     private RaycastHit hit;
-    private CleanDraw cleanDraw;
     private LayerMask layerMask;
+    private CleanDraw cleanDraw;
 
     [SerializeField]
     private float shotRange;
-
+    [SerializeField]
+    private float offsetY;
     private void Start()
     {
         layerMask = ~LayerMask.GetMask("Ground");
+        transform.localRotation = Quaternion.Euler(new Vector3(0, offsetY, 0));
     }
 
     private void Update()
     {
         if (Physics.Raycast(transform.position, transform.forward, out hit, shotRange, layerMask))
         {
-            Debug.DrawRay(transform.position, transform.forward * shotRange, Color.red);
-            cleanDraw = hit.collider.gameObject.GetComponent<CleanDraw>();
-            if(cleanDraw != null)
+            hit.transform.TryGetComponent<CleanDraw>(out cleanDraw);
+            if(cleanDraw)
             {
                 cleanDraw.Wash(hit);
             }
+            Debug.DrawRay(transform.position, transform.forward * shotRange, Color.green);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.forward * shotRange, Color.green);
+            Debug.DrawRay(transform.position, transform.forward * shotRange, Color.red);
         }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * shotRange);
     }
 }
