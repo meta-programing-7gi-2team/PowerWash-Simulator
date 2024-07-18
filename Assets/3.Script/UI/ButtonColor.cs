2081b613188114a3b2119c3d358a4094a82a852c
강유현 <soyuh1113@gmail.com>
2024-07-18 15:44:58 +0900

    [Update] 인 게임 UI 추가

--- Assets/3.Script/UI/ButtonColor.cs
+++ Assets/3.Script/UI/ButtonColor.cs
@@ -5,9 +5,31 @@ using UnityEngine.UI;
 
 public class ButtonColor : MonoBehaviour
 {
+    public Toggle toggle;
+    public Text text;
 
+    public Color DeselectedColor = new Color(22, 145, 255);
 
+    private void Start()
+    {
+        toggle.onValueChanged.AddListener(ChangeTextColor);
+        ChangeTextColor(toggle.isOn);
+    }
 
+    public void ChangeTextColor(bool isOn)
+    {
+        if (isOn)
+        {
+            text.color = Color.white;
+        }
+        else
+        {
+            text.color = DeselectedColor;
+        }
+    }
 
-
+    private void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(ChangeTextColor);
+    }
 }
