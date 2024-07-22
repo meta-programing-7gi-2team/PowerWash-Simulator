00e5a757a089a766dd82b826204b46c0284c0ae0
강유현 <soyuh1113@gmail.com>
2024-07-22 09:47:31 +0900

    [Update] 인 게임 UI 수정

---
+++ Assets/3.Script/UI/InGame/Shortcut_Key_Control.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shortcut_Key_Control : MonoBehaviour
+{
+    [Header("»Æ¿Â±‚")]
+    [SerializeField] private ToggleController E_toggle1;
+    [SerializeField] private ToggleController E_toggle2;
+    [SerializeField] private ToggleController E_toggle3;
+    [SerializeField] private ToggleController E_toggle4;
+
+    [Header("≥Î¡Ò")]
+    [SerializeField] private ToggleController N_toggle1;
+    [SerializeField] private ToggleController N_toggle2;
+    [SerializeField] private ToggleController N_toggle3;
+    [SerializeField] private ToggleController N_toggle4;
+
+    private void Start()
+    {
+        
+    }
+
+
+}
